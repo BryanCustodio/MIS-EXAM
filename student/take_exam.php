2d2749844346729ee5f624bf421f51ce5d4652ae
Bryshiee <159089275+BryanCustodio@users.noreply.github.com>
2025-03-26 13:44:26 +0800

    g

--- student/take_exam.php
+++ student/take_exam.php
@@ -126,3 +126,5 @@ $exams = $examResult->fetch_all(MYSQLI_ASSOC);
     .modal-content { background-color: white; margin: 10% auto; padding: 20px; border-radius: 5px; width: 50%; }
     .close { float: right; font-size: 24px; cursor: pointer; }
 </style>
+
+<!-- <script src="../student-js/submit_exam.js"></script> -->
